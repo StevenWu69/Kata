3eac5843abe639dc2bef637a0ec0b9285f4faa9c
Steven Wu <steven.wu@titansoft.com.sg>
2020-06-09 22:31:05 +0800

    Kata2

---
+++ Kata/Kata/Kata2/Kata2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Kata.Kata2
+{
+    internal class Kata2
+    {
+        public static int DeadAnts(string str)
+        {
+            if (str == null)
+            {
+                return 0;
+            }
+
+            int Head = 0;
+            int Body = 0;
+            int Tail = 0;
+
+            var saveTheAnts = Regex.Split(str, "ant");
+            foreach (var body in saveTheAnts)
+            {
+                for (int i = 0; i < body.Length; i++)
+                {
+                    if (body[i] == 'a')
+                    {
+                        Head++;
+                    }
+                    else if (body[i] == 'n')
+                    {
+                        Body++;
+                    }
+                    else if (body[i] == 't')
+                    {
+                        Tail++;
+                    }
+                }
+            }
+
+            return Math.Max(Head, Math.Max(Body, Tail));
+
+        }
+    }
+}
---
+++ Kata/Kata/Kata2/Kata2Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Kata2
+{
+    [TestFixture]
+    public class Kata2Test
+    {
+        [Test]
+        public void DeadAntsAllSurvive()
+        {
+            Assert.AreEqual(0, Kata2.DeadAnts(".ant..ant..."));
+        }
+
+        [Test]
+        public void NoAnts()
+        {
+            Assert.AreEqual(0, Kata2.DeadAnts(""));
+        }
+
+        [Test]
+        public void Blank()
+        {
+            Assert.AreEqual(0, Kata2.DeadAnts("   "));
+        }
+
+        [Test]
+        public void Ants4()
+        {
+            Assert.AreEqual(4, Kata2.DeadAnts("..aant a nt an t anttt "));
+        }
+
+        [Test]
+        public void Ants2()
+        {
+            Assert.AreEqual(2, Kata2.DeadAnts("ant anantt aantt "));
+        }
+    }
+}
