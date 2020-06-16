b8e77166bd35ce210a7e8621ffc0317df3f81658
Steven Wu <steven.wu@titansoft.com.sg>
2020-06-16 10:34:42 +0800

    Kata3

---
+++ Kata/Kata/Kata3/Kata3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Kata.Kata3
+{
+    internal class Kata3
+    {
+        public static string BandNameGenerator(string str)
+        {
+            var regexItem = new Regex("^[a-zA-Z]*$");
+
+            if (string.IsNullOrEmpty(str) || str[0] == ' ')
+            {
+                return "invalid input";
+            }
+
+            char head = str[0];
+            char tail = str[str.Length - 1];
+
+            if (str.Length == 1)
+            {
+                if (!regexItem.IsMatch(str[0].ToString()))
+                {
+                    return "The " + str;
+                }
+                return "The " + str.ToUpper();
+            }
+
+            if (head == tail)
+            {
+                if (!regexItem.IsMatch(str[0].ToString()))
+                {
+                    return str + str.Remove(0, 1);
+                }
+                return str[0].ToString().ToUpper() + str.Remove(0, 1) + str.Remove(0, 1);
+            }
+
+            string UpperHeadstr;
+
+            if (!regexItem.IsMatch(str[0].ToString()))
+            {
+                return "The " + str;
+            }
+
+            UpperHeadstr = str[0].ToString().ToUpper() + str.Remove(0,1);
+
+            return "The " + UpperHeadstr;
+
+        }
+    }
+}
---
+++ Kata/Kata/Kata3/Kata3Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Kata3
+{
+    [TestFixture]
+    public class Kata3Test
+    {
+        [Test]
+        public void NormalName()
+        {
+            Assert.AreEqual("The Abc", Kata3.BandNameGenerator("abc"));
+        }
+
+        [Test]
+        public void HeadEqualstoTail()
+        {
+            Assert.AreEqual("Abcabca", Kata3.BandNameGenerator("abca"));
+        }
+
+        [Test]
+        public void SpecialHead1()
+        {
+            Assert.AreEqual("The !abc", Kata3.BandNameGenerator("!abc"));
+        }
+
+        [Test]
+        public void SpecialHead2()
+        {
+            Assert.AreEqual("The 0abc", Kata3.BandNameGenerator("0abc"));
+        }
+
+        [Test]
+        public void BlankInput()
+        {
+            Assert.AreEqual("invalid input", Kata3.BandNameGenerator(" "));
+        }
+
+        [Test]
+        public void BlankWithName()
+        {
+            Assert.AreEqual("invalid input", Kata3.BandNameGenerator(" abc"));
+        }
+
+        [Test]
+        public void SingleInput1()
+        {
+            Assert.AreEqual("The A", Kata3.BandNameGenerator("a"));
+        }
+
+        [Test]
+        public void SingleInput2()
+        {
+            Assert.AreEqual("The ?", Kata3.BandNameGenerator("?"));
+        }
+    }
+}
